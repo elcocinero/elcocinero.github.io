0c28ab40b5bd842bf61013a6d1ae78e83122644b
Ryan Cook <ryanfranciscook@gmail.com>
2015-12-20 17:31:15 -0500

    more photos

--- ryanmap/prep code.R
+++ ryanmap/prep code.R
@@ -21,11 +21,9 @@ unique.trips <- unique(unique.trips)
 # using: http://www.gpsvisualizer.com/convert_input?convert_format=gpx
 trail.names <- list.files(path="gpx")
 
-
 for (i in 1:length(trail.names)) {
   assign(paste("GPX",i,".gpx",sep=""), readOGR(paste("gpx/",trail.names[i],sep=""), layer = "tracks"))
 }
-
 
 
 #getting picture info
@@ -54,15 +52,31 @@ exif_long <- function(path) {
 }
 
 for (i in 1:length(photo.df$photo.names)) {
-  photo.df$time[i] <- exif_time(paste("photos/",photo.df$photo.names[i], sep=""))
+  photo.df$date[i] <- substr(exif_time(paste("photos/",photo.df$photo.names[i], sep="")),1,10)
   photo.df$lat[i] <- as.numeric(exif_lat(paste("photos/",photo.df$photo.names[i], sep="")))
   photo.df$long[i] <- as.numeric(exif_long(paste("photos/",photo.df$photo.names[i], sep="")))
 }
 
+# correcting photo errors
+PhotoEdits <- read.csv("PhotoEdits.csv",stringsAsFactors=FALSE, header=TRUE)
+DateEdits <- PhotoEdits[PhotoEdits$Year != ".",c("ImageID","Year","Month","Day")]
+CoordEdits <- PhotoEdits[PhotoEdits$Lat != ".",c("ImageID","Lat","Lng")]
+
+photo.df
+for (i in 1:length(DateEdits$ImageID)) {
+  photo.df$date[photo.df$photo.names == DateEdits$ImageID[i]] <-paste(DateEdits$Year[i],DateEdits$Month[i],DateEdits$Day[i], sep=":")
+}
+
+for (i in 1:length(CoordEdits$ImageID)) {
+  photo.df$lat[photo.df$photo.names == CoordEdits$ImageID[i]] <- CoordEdits$Lat[i]
+  photo.df$long[photo.df$photo.names == CoordEdits$ImageID[i]] <- CoordEdits$Lng[i]
+}
+
+
 photo.df
 
+# adding month of photo
 months <- c("Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec")
-
 photo.df$month <- paste(months[as.numeric(substr(photo.df$time,6,7))],substr(photo.df$time,1,4),sep=" ")
 photo.df$monthnum <- as.numeric(substr(photo.df$time,1,4)) + (as.numeric(substr(photo.df$time,6,7))/100)
 
@@ -72,58 +86,45 @@ unique.months <- unique.months[order(unique.months)]
 unique.months <- paste(months[as.numeric(substr(unique.months,6,7))],substr(unique.months,1,4),sep=" ")
 
 
-# creating icon
+# creating photo icon
 photoIcon <- makeIcon(
   iconAnchorX = 12, iconAnchorY = 12, # center middle of icon on track,
   # instead of top corner  
   iconUrl = "https://www.mapbox.com/maki/renders/camera-12@2x.png"
 )
 
-cityIcon <- makeIcon(
-  iconAnchorX = 12, iconAnchorY = 12, # center middle of icon on track,
-  # instead of top corner  
-  iconUrl = "http://www.capitolscientific.com/core/media/media.nl?id=453195&c=1250437&h=f177909298363d4e354b"
-)
-
+# blank text to fill line below photo
 blanks="&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp"
 
-#creating map
-# mapStates <- map("state", fill=TRUE,
-#                  plot=FALSE,
-#                  region=c("California","Oregon","Washington"))
-
+#creating base map
 map <- leaflet() %>%
   setView(lat = 39.50, lng = -98.35, zoom = 5) %>%
   addProviderTiles("MapBox.ryancook.o8im6llh") %>%
-#   addPolylines (color="red", popup="PCT") %>%
-#   addPolygons(data=mapStates, stroke=FALSE, fillColor="#373737") %>%
-#   addLegend(position="topright",colors="red", labels="PCT", opacity=0.2, title="Trail Map") %>%
-  
-  
-#   for (i in 1:length(photo.df$photo.names)) {
-#       addMarkers(photo.df$long[i], photo.df$lat[i], icon=photoIcon, group='Photos', popup=photo.df$time[i]) %>%
-#   }
 
-  
+# adding legend
+#   addLegend(position="topright",colors="red", labels="PCT", opacity=0.2, title="Trail Map") %>%
+
+
+# adding control box
   addLayersControl(
     overlayGroups=c("Cities","Photos","Plane","Car"),
     options = layersControlOptions(collapsed=FALSE)
   )
 
-  map <- addPolylines(map,lng=trip.markers$Lng, lat=trip.markers$Lat, color="black", opacity=0.3, weight=2, group="Plane")
+#adding plane trips
+map <- addPolylines(map,lng=trip.markers$Lng, lat=trip.markers$Lat, color="black", opacity=0.3, weight=2, group="Plane")
 
-#   map <- addPolylines(map,data=trail, color="black", opacity=0.3, weight=2, group="Car")
-
+#adding car rides
 for (i in 1:length(trail.names)) {
   map <- addPolylines(map,data=get(trail.names[i]), color="black", opacity=0.3, weight=2, group="Car")
 }
 
-
+#adding city markers
 for (i in 1:length(unique.trips$City)) {
   map <- addCircleMarkers(map, lng=unique.trips$Lng[i], lat=unique.trips$Lat[i], group="Cities", popup=unique.trips$City[i])
 }
 
-
+# adding photos
   for (i in 1:length(photo.df$photo.names)) {
       map <- addMarkers(map, lng=photo.df$long[i], lat=photo.df$lat[i], icon=photoIcon, group="Photos", popup=
                           paste(
